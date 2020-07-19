58be5b7bfdd17038d08a5dd086946a560aeae1bf
Stephen Young <stephen.young.dev@gmail.com>
2020-07-19 14:52:06 -0500

    add templates for better user experience

--- auto_fork/app.py
+++ auto_fork/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, session, request
+from flask import Flask, session, request, make_response, render_template
+from werkzeug import exceptions
 from requests import post
 from urllib import parse
 from waitress import serve
 import uuid
+
 from config import AppConfig
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(AppConfig.flask)
 
 
@@ -22,7 +24,7 @@ def index():
               'redirect_uri': auth_endpoint}
     authorize_url = f"{AppConfig.github.authorize_endpoint}?" + parse.urlencode(params)
 
-    return f'<a href="{authorize_url}">Click Here to Surrender to Auto Fork</a>'
+    return make_response(render_template('index.html', auth_url=authorize_url))
 
 
 @app.route("/auth", methods=['GET'])
@@ -31,7 +33,13 @@ def auth():
     state = request.args.get("state")
 
     # verify this is a response to our request
-    if state == session.pop('auto_fork_state') and code is not None:
+    state_good = state == session.pop('auto_fork_state') if 'auto_fork_state' in session else False
+
+    if not state_good or not code:
+        raise exceptions.NotAcceptable
+    elif request.method != 'GET':
+        raise exceptions.MethodNotAllowed
+    else:
         headers = {'Accept': 'application/json'}
         payload = {'code': code,
                    'client_id': AppConfig.github.client_id,
@@ -39,21 +47,56 @@ def auth():
                    'scope': 'public_repo'}
         res = post(AppConfig.github.token_endpoint, headers=headers, json=payload)
 
-        data = res.json()
-        access_token = data['access_token']
-        session['github_access_token'] = access_token
-        fork_endpoint = parse.urljoin(request.base_url, '/fork')
-        return f'<a href="{fork_endpoint}">Click Here to Fork The Code For This App</a>'
-    else:
-        return "Request could not be validated"
+        if res.status_code == 200:
+            data = res.json()
+            access_token = data['access_token']
+            session['github_access_token'] = access_token
+            fork_endpoint = parse.urljoin(request.base_url, '/fork')
+            return make_response(render_template('authorized.html', fork_endpoint=fork_endpoint))
+        elif res.status_code == 403:
+            raise exceptions.Forbidden
+        else:
+            raise exceptions.ServiceUnavailable
 
 
 @app.route("/fork", methods=['GET'])
 def fork():
-    access_token = session['github_access_token']
-    headers = {'Authorization': f'token {access_token}'}
-    res = post(AppConfig.github.fork_endpoint, headers=headers)
-    return "forked"
+    if request.method != 'GET':
+        raise exceptions.MethodNotAllowed
+    elif 'github_access_token' not in session:
+        raise exceptions.Forbidden
+    else:
+        access_token = session['github_access_token']
+        headers = {'Authorization': f'token {access_token}'}
+        forked_res = post(AppConfig.github.fork_endpoint, headers=headers)
+
+        if forked_res.status_code == 202:
+            base_url = parse.urljoin(request.base_url, '/')
+            response = make_response(render_template('forked.html', base_url=base_url))
+        elif forked_res.status_code == 403:
+            raise exceptions.Unauthorized
+        else:
+            raise exceptions.ServiceUnavailable
+
+    return response
+
+
+@app.errorhandler(exceptions.Unauthorized)
+@app.errorhandler(exceptions.NotAcceptable)
+@app.errorhandler(exceptions.Forbidden)
+@app.errorhandler(exceptions.ServiceUnavailable)
+@app.errorhandler(exceptions.MethodNotAllowed)
+def error_handler(error: exceptions.HTTPException):
+    base_url = parse.urljoin(request.base_url, '/')
+    template = render_template('error.html', error_name=error.name, error_desc=error.description, base_url=base_url)
+    return make_response(template, error.code)
+
+
+@app.errorhandler(exceptions.NotFound)
+def not_found(error: exceptions.HTTPException):
+    base_url = parse.urljoin(request.base_url, '/')
+    template = render_template('not_found.html', base_url=base_url)
+    return make_response(template, error.code)
 
 
 if __name__ == "__main__":
